2b147eefc815312b604dc42bfe19885e8d8bf638
Omar <martinez.trujillo.omar@gmail.com>
2020-10-01 20:05:56 +0200

    add changeAnime to processCommand

--- src/main/java/com/omar/App.java
+++ src/main/java/com/omar/App.java
@@ -1,6 +1,7 @@
 package com.omar;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -112,12 +113,58 @@ public class App {
                 break;
 
             case "change": //TODO: Change
-                //After AniList changes → updateAnimeFile
+                changeAnime(aniList, tokens);
                 break;
 
             default:
                 System.out.println("Unknown command " + command);
                 break;
+        }
+    }
+
+    private static void changeAnime(AniList aniList, String[] tokens) {
+        try {
+            int animeId = Integer.parseInt(tokens[1]);
+            Anime toChange = aniList.getAniList().get(animeId + 1);
+
+            for (int i = 0; i < tokens.length; i++) {
+                switch (tokens[i]) {
+                    case "-status":
+                        Status newStatus = Status.valueOf(tokens[i + 1]);
+                        toChange.setStatus(newStatus);
+                        break;
+                    case "-score":
+                        int newScore = Integer.parseInt(tokens[i + 1]);
+                        if (newScore >= 0 && newScore <= 10) {
+                            toChange.setScore(newScore);
+                        } else {
+                            System.out.println("Score must be between 0 an 10");
+                        }
+                        break;
+
+                    case "-progress":
+                        int newProgress = Integer.parseInt(tokens[i + 1]);
+                        if (newProgress >= 0 && newProgress <= toChange.getEpisodes()) {
+                            toChange.setProgress(newProgress);
+                        } else {
+                            System.out.println("Progress must be between 0 and " + toChange.getEpisodes());
+                        }
+
+                        break;
+                }
+            }
+
+            aniList.updateAnimeList(toChange);
+            aniList.updateAnimeFile();
+
+        } catch (NumberFormatException numE) {
+            System.out.println("Please insert only numbers after change/progress/score");
+        } catch (IndexOutOfBoundsException indE) {
+            System.out.println(tokens[1] + " is not a valid id");
+        } catch (IllegalArgumentException illE) {
+            System.out.println("Status is not a valid");
+        } catch (IOException e) {
+            System.out.println("Anime file not found");
         }
     }
 
